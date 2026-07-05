5a84210b5d8ba5aeb7cb17078065d9106e00410b
aryaansaini <aryansaini2492004@gmail.com>
2026-07-06 01:40:19 +0530

    Complete JWT authentication and user profile API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,4 +10,6 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
 
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,28 @@
 from rest_framework import generics
 from .models import User
 from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        return Response({
+            "id": user.id,
+            "email": user.email,
+            "username": user.username,
+            "bio": user.bio,
+        })
